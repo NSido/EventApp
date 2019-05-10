e0844c4cc0dd8f7ba548d23e41c1cd6c8742b69a
Administrator <noor.117@hotmail.com>
2019-05-10 13:56:41 +0200

    try2

--- app/src/main/java/com/example/logindemo/HomeActivity.java
+++ app/src/main/java/com/example/logindemo/HomeActivity.java
@@ -2,12 +2,28 @@ package com.example.logindemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
